72b3e05ed22181cd43cc628cf5de6c93311ab9f7
NathanCardoso <nathan.cardoso@codezone.com.br>
2022-08-21 20:29:55 -0300

    searchProducts

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,37 +1,49 @@
 <template>
   <section class="container">
-		<div v-for="product in products" :key="product.id">
-		<img v-if="product.foto" :src="product.foto[0].src" :alt="product.foto[0].titulo">
-			<p class="price">{{product.preco}}</p>
-			<h2 class="title">{{product.nome}}</h2>
-			<p>{{product.descricao}}</p>
-		</div>
-	</section>
+    <div v-for="product in products" :key="product.id">
+      <img v-if="product.foto" :src="product.foto[0].src" :alt="product.foto[0].titulo" />
+      <p class="price">{{ product.preco }}</p>
+      <h2 class="title">{{ product.nome }}</h2>
+      <p>{{ product.descricao }}</p>
+    </div>
+    {{ url }}
+  </section>
 </template>
 
 <script>
-import {api} from "@/services.js"
+import { api } from "@/services.js";
+import {serialize} from '@/helpers.js'
 
 export default {
-	name: 'ProductList',
-	data() {
-		return {
-			products: null
+  name: "ProductList",
+  data() {
+    return {
+      products: null,
+			pageProducts: 9
+    };
+  },
+  computed: {
+    url() {
+			const query = serialize(this.$route.query)
+      return `/produto?_limit=${this.pageProducts}${query}`;
+    },
+  },
+	watch: {
+		url() {
+			this.getProducts()
 		}
 	},
-	methods: {
-		getProducts() {
-			api.get('/produto')
-			.then(responseJson => {
-				this.products = responseJson.data		
-			})
-		}
-	},
-	created() {
-		this.getProducts()
-	}
-}
+  methods: {
+    getProducts() {
+      api.get(this.url).then((responseJson) => {
+        this.products = responseJson.data;
+      });
+    },
+  },
+  created() {
+    this.getProducts();
+  },
+};
 </script>
 
-<style>
-</style>
+<style></style>
--- src/components/searchProducts.vue
+++ src/components/searchProducts.vue
@@ -1,10 +1,24 @@
 <template>
-	<div></div>
+	<form>
+		<input id="search" name="search" type="text" v-model="search">
+		<input type="submit" id="magnifier" value="Buscar" @click.prevent="searchProduct">
+		{{search}}
+	</form>
 </template>
 
 <script>
 	export default {
-		name: 'SearchProducts'
+		name: 'SearchProducts',
+		data() {
+			return {
+				search: ""
+			}
+		},
+		methods: {
+			searchProduct() {
+				this.$router.push({query: {q: this.search}})
+			}
+		},
 	}
 </script>
 
